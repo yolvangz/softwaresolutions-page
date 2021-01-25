9d39506de3985721fa5251b7701752a9a48b25e0
Yolvan G. Zambrano <yolvangzg@gmail.com>
2021-01-25 15:33:01 -0400

    Added desktop animations to rest of landing page

--- js/modules/animations.js
+++ js/modules/animations.js
@@ -3,8 +3,19 @@ export function landingAnimations (){
 	const hero = [
 		document.querySelector('#hero .hero-title'),
 		document.querySelector('#hero .hero-title + p.lead')
+	];
+	const sectionItems = [
+		document.querySelector('#quienes-somos .section-title'),
+		document.querySelector('#quienes-somos .section-description')
+	];
+	const sections = [
+		document.querySelector('#quienes-somos'),
+		document.querySelector('#servicios'),
+		document.querySelector('#contacto'),
+		document.querySelector('#mapa'),
+		document.querySelector('footer')
 	]
-	
+
 	// Navbar & Hero
 	sr.reveal('.navbar', {
 		origin: 'bottom',
@@ -31,5 +42,43 @@ export function landingAnimations (){
 		delay: 1000,
 		distance: '50px',
 		interval: 400,
+	});
+
+	// Parallax, sections
+	sr.reveal(sections, {
+		origin: 'bottom',
+		duration: 1000,
+		viewFactor: 0.1
+	});
+	sr.reveal('.parallax', {
+		origin: 'bottom',
+		duration: 2000,
+		viewFactor: 0.3
+	});
+	sr.reveal('.parallax-text', {
+		origin: 'bottom',
+		duration: 2000,
+		distance: '100px',
+		viewFactor: 1
+	});
+	sr.reveal('#quienes-somos .container', {
+		container: '#quienes-somos',
+		origin: 'bottom',
+		duration: 2000
+	});
+	sr.reveal(sectionItems, {
+		origin: 'top',
+		duration: 2000,
+		distance: '50px',
+		viewFactor: 0.1,
+		interval: 400
+	});
+	// Services
+	sr.reveal('#servicios .grid-item', {
+		origin: 'top',
+		duration: 2000,
+		distance: '20px',
+		interval: 333,
+		viewFactor: 0.2
 	});
 }
